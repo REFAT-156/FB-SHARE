1a11dac793f799471326f0eae6c4f37cf9d540a0
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2023-01-07 00:32:14 +0600

    Update Share.py

--- Share.py
+++ Share.py
@@ -1,5 +1,27 @@
-import os, sys
-try:
-    __import__("share").bot()
-except Exception as e:
-    exit(str(e))
+import os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.06)
+xoss('\n\x1b[1;37m[●] Checking Update........✔️✔️');time.sleep(0.5)
+def Update():
+    exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🍼🙂")
+            xoss('\x1b[1;94m[●] Follow My Github First 🎈')
+            os.system('xdg-open https://github.com/REFAT-156')
+            from share import refat_share
+            refat_share()
+        elif bit == '32bit':
+            xoss("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🍼🙂")
+            xoss('\x1b[1;94m[●] Follow My Github First 🎈')
+            os.system('xdg-open https://github.com/REFAT-156')
+            from share import refat_share
+            refat_share()
+        else:
+            exit('\033[1;31m[●] Connection & Network Error 🤕')
+Run()
+ 
